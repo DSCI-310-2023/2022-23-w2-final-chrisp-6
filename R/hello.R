0191139c155c1dfeda14921bad8250aa678ab399
chrisp-6 <88063400+chrisp-6@users.noreply.github.com>
2023-04-18 19:04:25 -0700

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' Hello
+#'
+#' You do not need to unit test this function
+#'
+#' This function should exist in the NAMESPACE
+#'
+#'
+#' @examples
+#'
+#' # this calls the function and should be working
+#' hello_dsci310_class()
+#'
+hello <- function() {
+  print("Hello Folks!")
+}
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,5 @@
+# do not need to modify this file, it is here for reference
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
